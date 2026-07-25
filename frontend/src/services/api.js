b5810dedabfd489bdc680b7623af645928b82ec5
Swati Giri <swatigoswami8800@gmail.com>
2026-07-25 19:32:46 +0530

    fiex direct  base url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL || "https://career-accelerator-62us.onrender.com/api",
+  baseURL: "https://career-accelerator-62us.onrender.com/api",
 });
 
 api.interceptors.request.use((config) => {
